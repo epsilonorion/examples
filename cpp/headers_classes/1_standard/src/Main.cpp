127b8971a96509695d6e6d375a92bfc457f3d737
Joshua Weaver <epsilonorion@gmail.com>
2019-05-23 14:50:42 -0500

    Updated main.cpp of 1_standard within headers_classes tutorial

--- cpp/headers_classes/1_standard/src/Main.cpp
+++ cpp/headers_classes/1_standard/src/Main.cpp
@@ -12,7 +12,8 @@ void doStuff();
 void doMoreStuff();
 void doAwesomeStuff();
 
-int main(int argc, char * argv[]) {
+int main(int argc, char * argv[]) 
+{
   doStuff();
 
   // Redefined global iVal in main function after doStuff called
@@ -30,11 +31,23 @@ int main(int argc, char * argv[]) {
   sleep(2);
 
   doAwesomeStuff();
+
+  return 0;
 }
 
 void doStuff()
 {
   std::cout << "This function will do stuff..." << std::endl << std::endl;
+
+  if (iVal == 12)
+  {
+    std::cout << "Value was 12" << std::endl;
+  }
+  else
+  {
+    std::cout << "Value was not 12" << std::endl;
+  }
+  
 
   std::cout << "Such as display global iVal: " << iVal << std::endl << std::endl;
 }
@@ -44,6 +57,13 @@ void doMoreStuff()
   /// Defined global dVal in doMoreStuff function
   dVal = 20.42;
 
+  for (int i = 0; i < 5; i++)
+  {
+    std::cout << "DVal is equal to: " << dVal + i << std::endl;
+  }
+
+  std::cout << "Dval is currently equal to: " << dVal << std::endl;
+
   std::cout << "This function will do more stuff" << std::endl << std::endl;
 
   std::cout << "Such as display the new global iVal: " << iVal << std::endl;
@@ -52,6 +72,14 @@ void doMoreStuff()
 
 void doAwesomeStuff()
 {
+  int j = 5;
+  while (j > 0)
+  {
+    dVal = dVal - j;
+    std::cout << "Dval is now: " << dVal << std::endl;
+    j--;
+  }
+
   std::cout << "This function knows how to do stuff, " << std::endl;
   std::cout << "it will have the best stuff." << std::endl;
   std::cout << "It is the best, but there is no better " << std::endl;
